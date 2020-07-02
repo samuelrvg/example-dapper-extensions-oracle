860493deb6c30d98072e56edbb8b5c5507c64a86
samuel.rvg <samuel.rvg@gmail.com>
2020-07-02 01:51:39 -0400

    example using dapper extensions oracle

---
+++ ExampleDapperExtensionsOracle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Dapper;
+using DapperExtensions.Oracle;
+using Oracle.ManagedDataAccess.Client;
+
+namespace ExampleDapperExtensionsOracle
+{
+    //[Table(TableName = "dbo.People", KeyName = "PeopleId", SequenceName = "SEQ_PEOPLE", IsIdentity = false)]
+    [Table(TableName = "People", KeyName = "PeopleId", SequenceName = "SEQ_PEOPLE", IsIdentity = false)]
+    public class People
+    {
+        public int PeopleId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new People();
+            people.Name = "Samuel";
+            people.Age = 30;
+
+            using (var con = new OracleConnection("your connection string"))
+            {
+                con.Insert(people);
+                con.Update(people);
+                var result = con.Query("select * from people where peopleid = 1");
+                con.Delete<People>(1);
+
+                //......
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
